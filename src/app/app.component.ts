4d7cd96e8a7f1eaaff683ad1dc5e917a5ac3c2a7
bczmarko <bczmarko@intrend.hu>
2024-01-02 14:03:44 +0100

    menu-bar component unnecessary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./common/header/header.component";
-import {MenuBarComponent} from "./common/menu-bar/menu-bar.component";
 import {FooterComponent} from "./common/footer/footer.component";
 import {AuthService} from "./auth/service/auth.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, MenuBarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
